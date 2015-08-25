5dc09e6b4e29f76b5004faf6fdd070bce3b6574d
dwlim12 <dwlim@NN121-HA-07.HS.sd203.org>
2015-08-25 10:08:45 -0500

    Mind Blown
    Animated gifs work

---
+++ Chapter 1 Practice/Exercise17.java
@@ -0,0 +1,14 @@
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class Exercise17
+{
+    public static void main(String[] args) throws Exception
+    {
+        URL imageLocation = new URL(
+            "https://s-media-cache-ak0.pinimg.com/originals/b2/8a/3a/b28a3a52ee99399a5389e758f7de87b6.gif");
+        JOptionPane.showMessageDialog(null, "Hi", "Title",
+            JOptionPane.PLAIN_MESSAGE, new ImageIcon(imageLocation));
+    }
+}
