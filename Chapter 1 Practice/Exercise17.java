73a884581cadfadf1ea7947606cbc94bfc44f7e7
dwlim12 <dwlim@NN121-HA-07.HS.sd203.org>
2015-08-31 10:29:26 -0500

    cool

--- Chapter 1 Practice/Exercise17.java
+++ Chapter 1 Practice/Exercise17.java
@@ -8,7 +8,7 @@ public class Exercise17
     {
         URL imageLocation = new URL(
             "https://s-media-cache-ak0.pinimg.com/originals/b2/8a/3a/b28a3a52ee99399a5389e758f7de87b6.gif");
-        JOptionPane.showMessageDialog(null, "Hi", "Title",
+        JOptionPane.showMessageDialog(null, "Mind Blown", "Title",
             JOptionPane.PLAIN_MESSAGE, new ImageIcon(imageLocation));
     }
 }
