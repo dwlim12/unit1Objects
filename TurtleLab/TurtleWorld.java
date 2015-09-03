831a9b7116d902a044bbd624ddbfb71d4582d256
dwlim12 <dwlim@NN121-HA-07.HS.sd203.org>
2015-09-03 10:25:05 -0500

    Turtle Lab
    I couldn't make it change colors right :(

--- TurtleLab/TurtleWorld.java
+++ TurtleLab/TurtleWorld.java
@@ -5,12 +5,45 @@ public class TurtleWorld
     public static void main(String[] args)
     {
         World myWorld = new World();
-        Turtle squirtle = new Turtle(myWorld);
-        squirtle.setPenColor(Color.RED);
-        squirtle.penDown();
-        squirtle.forward(2000);
-        Turtle skittles = new Turtle(myWorld);
+        Turtle squirtle = new Turtle(0,0,myWorld);
+        Turtle skittles = new Turtle(750,750,myWorld);
+        for (int i=0; i<96; i++){
+        Color pencolor = Color.RED;
+        int colornumber = i*6;
+        if (colornumber == 6){ pencolor = Color.RED;}
+        if (colornumber == 12){ pencolor = Color.BLUE;}
+        if (colornumber == 18){ pencolor = Color.GREEN;}
+        if (colornumber == 24){ pencolor = Color.YELLOW;}
+        if (colornumber == 30){ pencolor = Color.ORANGE;}
+        if (colornumber == 36){ pencolor = Color.PINK;}
+        if (colornumber == 42){ pencolor = Color.MAGENTA;}
+        if (colornumber == 48){ pencolor = Color.CYAN;}
         squirtle.turnRight();
-        
+        squirtle.setPenWidth(3);
+        squirtle.setPenColor(pencolor);
+        squirtle.penDown();
+        squirtle.forward(750);
+        squirtle.turnRight();
+        squirtle.forward(750);
+        squirtle.turnRight();
+        squirtle.forward(315);
+        squirtle.turnRight();
+        squirtle.penUp();
+        squirtle.forward((i*5)+5);
+        squirtle.turnLeft();
+        squirtle.turnLeft();
+        skittles.turnLeft();
+        skittles.setPenWidth(3);
+        skittles.setPenColor(pencolor);
+        skittles.penDown();
+        skittles.forward(750);
+        skittles.turnRight();
+        skittles.forward(750);
+        skittles.turnRight();
+        skittles.forward(325);
+        skittles.turnRight();
+        skittles.penUp();
+        skittles.forward((i*5)+5);
+    }
     }
 }
